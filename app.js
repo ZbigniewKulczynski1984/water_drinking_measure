e01e2cc13ff55772c7b65171b7c35f3daaac4280
Zbigniew Kulczński <z-kulczynski@o2.pl>
2022-10-31 09:21:00 +0100

    code added on small cups

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ smallCups.forEach((cup, idx) => {
 })
 
 function highlightCups(idx) {
+    if(smallCups[idx].classList.contains('full') && !smallCups[idx].nextElementSibling.classList.contains('full')) {
+        idx--
+    }
+
     console.log(idx)
     smallCups.forEach((cup, idx2) => {
         if(idx2 <= idx) {

0be85b5c3b00d91da32b6c27794cdaadcd021f67
Zbigniew Kulczński <z-kulczynski@o2.pl>
2022-10-31 09:52:36 +0100

    big cup styled

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const smallCups = document.querySelectorAll('.cup-small')
 const liters = document.getElementById('liters')
-const procentage = document.getElementById('procentage')
+const percentage = document.getElementById('procentage')
 const remained = document.getElementById('remained')
 
+updataBigCup()
+
 smallCups.forEach((cup, idx) => {
-    console.log(idx)
+    // console.log(idx)
     cup.addEventListener('click', () => highlightCups(idx))
 })
 
@@ -13,7 +15,7 @@ function highlightCups(idx) {
         idx--
     }
 
-    console.log(idx)
+    // console.log(idx)
     smallCups.forEach((cup, idx2) => {
         if(idx2 <= idx) {
             cup.classList.add('full') 
@@ -21,4 +23,28 @@ function highlightCups(idx) {
             cup.classList.remove('full') 
         }
     })
+
+    updataBigCup()
+}
+
+function updataBigCup() {
+    const fullCups = document.querySelectorAll('.cup-small').length
+    const totalCups = smallCups.length
+
+    if(fullCups === 0) {
+        percentage.style.visibility = 'hidden'
+        percentage.style.height = 0
+    } else {
+        percentage.style.visibility = 'visible'
+        percentage.style.height = `${fullCups / totalCups * 330}px`
+        percentage.innerText = `${fullCups / totalCups * 100}%`
+    }
+    if(fullCups === totalCups) {
+        remained.style.visibility = 'hidden'
+        remained.style.height = 0
+    } else {
+        remained.style.visibility = 'visible'
+        liters.innerText = `${2 - (250 * fullCups / 1000)}`
+    }
+    // console.log(fullCups)
 }
